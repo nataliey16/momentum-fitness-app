6aa50765e9cc0a89d63eedead98b123f29e45c77
Natalie <natalie.yeung16@gmail.com>
2024-08-04 13:32:11 -0600

    Create header component with css

---
+++ src/app/components/header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+const BUTTON_STYLES =
+  "absolute top-3/4 hover:bg-red-50 hover:text-slate-800 left-1/2 transform -translate-x-1/2 -translate-y-[80%] bg-slate-700 text-white px-10 py-4 rounded-full";
+
+const Header = () => {
+  return (
+    <div className="h-screen w-full grid grid-cols-2 items-center">
+      {" "}
+      <div className="flex-1 bg-[#e4c034]">
+        <h1 className="text-[#292524] text-9xl flex min-h-screen items-center justify-end p-0">
+          Mome
+        </h1>
+      </div>
+      <div className="flex-1 bg-[#292524] text-[#e4c034]">
+        <h1 className="text-[#e4c034] text-9xl flex min-h-screen items-center justify-start p-0">
+          ntum
+        </h1>
+      </div>
+      <div>
+        <h3 className="absolute inset-0 flex items-center justify-center text-3xl text-white mt-36">
+          Fuel Your Fitness Journey
+        </h3>{" "}
+      </div>
+      <Link href="/" className={BUTTON_STYLES}>
+        Get Started
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Bebas_Neue, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata = {
   title: "Momentum",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={bebasNeue.className}>{children}</body>
     </html>
   );
 }
